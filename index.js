098d070912f2b44ddfa6a43ca42016953922974b
何东升 <he77505357@163.com>
2021-12-08 17:58:56 +0800

    常量处理

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const _pending = 'pending';
+const _fulfilled = 'fulfilled';
+const _rejected = 'rejected';
+
 class Promise2 {
-    static pending = 'pending';
-    static fulfilled = 'fulfilled';
-    static _rejected = 'rejected';
+
+    static get _pending() {
+        return _pending;
+    }
+
+    static get _fulfilled() {
+        return _fulfilled;
+    }
+
+    static get _rejected() {
+        return _rejected;
+    }
+
     static resolve(value) {
         // 判断是否是thenable对象
         if (value instanceof Promise2 || ((typeof value === 'object') && 'then' in value)) {
@@ -17,8 +31,9 @@ class Promise2 {
             return value;
         }
 
-        return new Promise2( (resolve,reject) => reject(value));
+        return new Promise2((resolve, reject) => reject(value));
     }
+
     static all(iterable) {
         return new Promise2((resolve, reject) => {
             const ret = [];
@@ -41,12 +56,13 @@ class Promise2 {
         return new Promise2((resolve, reject) => {
             Array.from(iterable).forEach(item => {
                 Promise2.resolve(item).then(resolve, reject);
+                //Promise2.resolve(item).then((res)=> {resolve(res)}, (err)=>{reject(err)});
             });
         });
     }
 
     constructor(executor) {
-        this.status = Promise2.pending; // 初始化状态为pending
+        this.status = Promise2._pending; // 初始化状态为pending
         this.value = undefined; // 存储 this._resolve 即操作成功 返回的值
         this.reason = undefined; // 存储 this._reject 即操作失败 返回的值
         this.callbacks = [];
@@ -67,6 +83,7 @@ class Promise2 {
             });
         });
     }
+
     catch(onRejected) {
         return this.then(null, onRejected);
     }
@@ -74,6 +91,7 @@ class Promise2 {
     finally(onFinally) {
         return this.then(onFinally, onFinally);
     }
+
     _resolve(result) {
         if (result instanceof Promise2) {
             result.then(
@@ -83,7 +101,7 @@ class Promise2 {
             return;
         }
         this.value = result;
-        this.status = Promise2.fulfilled; // 将状态设置为成功
+        this.status = Promise2._fulfilled; // 将状态设置为成功
         this.callbacks.forEach((cb) => this._handler(cb));
     }
 
@@ -99,22 +117,23 @@ class Promise2 {
         this.status = Promise2._rejected; // 将状态设置为失败
         this.callbacks.forEach((cb) => this._handler(cb));
     }
+
     _handler(callback) {
-        const { onFulfilled, onRejected, nextResolve, nextReject } = callback;
-        if (this.status === Promise2.pending) {
+        const {onFulfilled, onRejected, nextResolve, nextReject} = callback;
+        if (this.status === Promise2._pending) {
             this.callbacks.push(callback);
             return;
         }
 
 
-        if (this.status === Promise2.fulfilled) {
+        if (this.status === Promise2._fulfilled) {
             // 传入存储的值
             // 未传入onFulfilled时，将undefined传入
             const nextValue = onFulfilled ? onFulfilled(this.value) : this.value;
             nextResolve(nextValue);
         }
 
-        if (this.status === Promise2._rejected ) {
+        if (this.status === Promise2._rejected) {
             // 传入存储的错误信息
             // 同样的处理
             const nextReason = onRejected ? onRejected(this.reason) : this.reason;
@@ -124,14 +143,49 @@ class Promise2 {
 }
 
 
+// console.log(Promise2.resolve(22).then(res => {
+//     console.log(res)
+// }))
+// console.log(Promise2.resolve(33).then((res => {
+//     throw new Error('Oh no!' + res);
+// })).catch(res => {
+//     console.log(res)
+// }))
+// console.log(new Promise2(() => {
+//     console.log(77)
+// }))
+//
+// const a = Promise2.all([Promise2.resolve(1), new Promise2((resolve)=>{
+//     setTimeout(()=> {resolve(55)},3000)
+// }), Promise2.rejected(11)]);
+// console.log('a')
+// console.log(a)
+// setTimeout(()=> {
+//     console.log('a-1')
+//     console.log(a)
+// }, 7000)
+//
+//
+// a.then(res => {
+//     console.log('res-a')
+//     console.log(res)
+// })
+
+const b = Promise2.race([Promise2.resolve(1), new Promise2((resolve)=>{
+    setTimeout(()=> {resolve(55)},3000)
+}), new Promise2((resolve, reject)=> {
+    setTimeout(()=> {reject('77-reject')})
+})]);
 
 
+console.log(b)
 
-
-Promise2.rejected(33).then(res => {
+b.then(res => {
+    console.log('res-race')
     console.log(res)
+}).catch(err=> {
+    console.log('res-race-catch')
+    console.log(err)
 })
-
-
 
 
